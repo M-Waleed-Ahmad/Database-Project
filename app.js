af44b40c55cc03767d5cd6c81885308249cbcee1
Nox <waleed.ahmadmunir@gmail.com>
2024-04-23 19:16:30 +0500

    Index commits

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const express = require('express');
-const path=require('path')
- const app = express();
+const path = require('path');
+const app = express();
 
 const port = 80;
 
+// Define paths for static files
+const publicDirectoryPath = path.join(__dirname, 'src');
 
-// Loading Css and images
-app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/css', express.static(__dirname + '/css'));
 app.use('/images', express.static(__dirname + '/images'));
 
-
-// Pug stuff
-app.set('view engine','pug');
-app.set('views',path.join(__dirname,'src'));
-
 // Endpoints
-app.get()
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: publicDirectoryPath });
+});
 
+app.get('/home', (req, res) => {
+    res.sendFile('home.html', { root: publicDirectoryPath });
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile('contact.html', { root: publicDirectoryPath });
+});
+
+app.get('/services', (req, res) => {
+    res.sendFile('services.html', { root: publicDirectoryPath });
+});
+
+app.get('/doctors', (req, res) => {
+    res.sendFile('doctors.html', { root: publicDirectoryPath });
+});
+
+app.get('/SignUp', (req, res) => {
+    res.sendFile('signup.html', { root: publicDirectoryPath });
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile('login.html', { root: publicDirectoryPath });
+});
 
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    console.log('__dirname:', __dirname);
-
 });
